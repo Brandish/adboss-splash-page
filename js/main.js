912ca88f944926da4c47adae6392742707948112
Jennifer Uvina <Jennifer.Uvina@blueye.com>
2016-01-14 11:57:53 -0600

    Basic stlying for all pages, missing "our-ads"

--- js/main.js
+++ js/main.js
@@ -30,18 +30,41 @@ var Home = (function(){
             $('#trigger').click(function(){
                 var logo$ = $('#trigger');
                 var phone$ = $('.peek');
-                var pre = 'animated';
+                var buttons$ = $('.download-buttons');
+                var text$ = $('.call-action');
+                var height = phone$.position();
+                var fadeSpeed = 500;
+                var phoneHeight = 502;
+                
+                //set the phone's top to its curent numerical value
+                //this gets around animation problems in Chrome and Safari
+                phone$.css('top',height.top+'px');
+                
                 //fade the brandish logo
-                logo$.fadeOut(450);
-                //transition phone to the middle
-                phone$.addClass('full-size').animate({
-                        top: '0px',
-                        bottom: '0px'
-                },900);
-                
-                
+                logo$.fadeOut(fadeSpeed);
+                //restore the phone's size and hide resulting overflow
+                $('body').css('overflow','hidden');
+                phone$.addClass('full-size');
 
-                
+                //slide in phone and trigger other effects after slide
+                phone$.animate({
+                    bottom: '0px',
+                    top : '0px'
+                },800, function(){
+                    height = phone$.offset();
+                    //place the download now text above the phone
+                    text$.css({
+                        'top' : (height.top-50)+'px',
+                        'left' : height.left+'px'
+                    });
+                    //place the download images below the phone
+                    buttons$.css({
+                        'top' : (height.top+phoneHeight)+'px'
+                    });
+                    //fade in the elements
+                    text$.fadeIn(fadeSpeed);
+                    buttons$.fadeIn(fadeSpeed);
+                });
             });
         }
     }
